24549df958d97b539fba3205f01ed77a011d9666
Robin :) <118724817+Rqbln@users.noreply.github.com>
2024-05-29 12:43:36 +0200

    update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,11 +7,11 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/login.php">Connexion / S'inscrire</a></li>
-                <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/pages/negotiations.php">Négociations et Offres en cours</a></li>
-            </ul>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/sale.php">Ajouter un article</a></li>
+                <?php if (!isset($_SESSION['user_id'])): ?>
+                    <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/login.php">Connexion / S'inscrire</a></li>
+                <?php endif; ?>
+                <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/pages/seller_space.php">Espace Vendeur</a></li>
+                <li class="nav-item"><a class="nav-link nav-hover-effect" href="/agora_ece/pages/negotiations.php">Négociations</a></li>
             </ul>
             <form class="d-flex">
                 <button class="btn btn-outline-dark me-2" type="button">
