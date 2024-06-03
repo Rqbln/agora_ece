907c5fb7c1efaa76b7cf1350994bdb772c994977
TXRTXE <87776455+TXRTXE@users.noreply.github.com>
2024-06-03 08:17:27 +0200

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -255,7 +255,8 @@ function displayCategoryProducts($categorie, $result, $is_admin, $user_id)
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
                 <h1 class="display-4 fw-bolder">Bienvenue sur Agora</h1>
-                <p class="lead fw-normal text-white-50 mb-0">Bienvenue sur la marketplace préférée des électroniciens !</p>
+                <p class="lead fw-normal text-white-50 mb-0">La marketplace préférée des électroniciens !</p>
+                <p class="lead fw-normal text-white-50 mb-0">Achetez et vendez des objets de valeur !</p>
             </div>
         </div>
     </header>
